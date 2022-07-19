db8baf3035024246f636c8ae2793546d3667e82e
tuncatunc <atuncatunc@gmail.com>
2022-07-19 14:28:04 +0300

    Procfile

--- app/database.py
+++ app/database.py
@@ -1,12 +1,10 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}
